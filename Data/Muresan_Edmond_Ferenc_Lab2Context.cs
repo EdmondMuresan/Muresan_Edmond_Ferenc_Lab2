eea4fc263d0662b61d2e815ee3e0beef89142814
thesh <thesh@DESKTOP-LDKMCPH>
2023-10-22 17:20:55 +0300

    Add project files.

---
+++ Data/Muresan_Edmond_Ferenc_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Muresan_Edmond_Ferenc_Lab2.Models;
+
+namespace Muresan_Edmond_Ferenc_Lab2.Data
+{
+    public class Muresan_Edmond_Ferenc_Lab2Context : DbContext
+    {
+        public Muresan_Edmond_Ferenc_Lab2Context (DbContextOptions<Muresan_Edmond_Ferenc_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Muresan_Edmond_Ferenc_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Muresan_Edmond_Ferenc_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Muresan_Edmond_Ferenc_Lab2.Models.Author>? Author { get; set; }
+    }
+}
